93e203349bc24c01076e7399d4fd8fb7c0d59a1e
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2019-11-21 22:32:08 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,9 +21,10 @@ function draw() {
   // image(imgcover.height, 0 , 0, imgsfondo.width, imgcover.height)1290, 1920
 
   image(imgcover, 0 , 0, width , height);
-  image(imgocchi, 0 + map(rotationY, 0, 360, 1, width), 0 + map(rotationX, -180, 180, 1, width), width , height)
+  image(imgocchi, 0 , 0 + map(rotationX, 0, -20,0,-20, [20,20]), width , height)
   image(imgsfondo, 0 , 0, width , height);
 }
+
 
 function touchMoved(){
   return false;
