f36c6ca9a53eb630ddac8f610e74f8e8eeb8c3e5
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2019-11-26 23:30:06 +0100

    gioconda

--- sketch.js
+++ sketch.js
@@ -19,6 +19,17 @@ function draw() {
   image(imgsfondo, 0 , 0, width , height);
   image(imgocchi, 0 + map(rotationY, -180, 180, -20, 20 ), 0 + map(rotationX, -180, 180, -20, 20 ) , width , height)
   image(imgcover, 0 ,0 , width , height);
+
+  var myText = "NOT ALL THEY SAY IS FALSE:";
+  var myText2 = "THIS EYES FOLLOW YOU";
+
+  textFont("Cormorant Garamond");
+  textAlign(CENTER);
+  textSize(60);
+  fill(233,216,157);
+  text(myText, width/2, height/1.3);
+  text(myText2, width/2, height/1.25);
+
 }
 
 
