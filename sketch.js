41d43b82d52b0c77ac47f29613de498e6193b9db
Beatrice Bazzan <akabeatricebazzan@gmail.com>
2019-11-26 23:07:37 +0100

    gioconda

--- sketch.js
+++ sketch.js
@@ -3,10 +3,10 @@ var imgocchi;
 var imgcover;
 
 function preload(){
-  // setShakeThreshold(20);//meno è grande questo numero piu sara sensibile
-  imgsfondo = loadImage("/assets/giocondasfondo.jpg");
-  imgocchi = loadImage("/assets/giocondaocchi.png");
-  imgcover = loadImage("/assets/giocondacover.png");
+
+  imgsfondo = loadImage("/assets/bg.png");
+  imgocchi = loadImage("/assets/eyes.png");
+  imgcover = loadImage("/assets/cover2.png");
 }
 
 function setup() {
@@ -15,14 +15,10 @@ function setup() {
 }
 
 function draw() {
-
-  // image(imgcover, 0 , 0, imgcover.width, imgcover.height)
-  // image(imgocchi, 0 + map(rotationY, 0, 360, 1, width), 0 + map(rotationX, -180, 180, 1, width), imgocchi.width, imgocchi.height)
-  // image(imgcover.height, 0 , 0, imgsfondo.width, imgcover.height)1290, 1920
-
-  image(imgcover, 0 , 0, width , height);
-  image(imgocchi, 0 , 0 + map(rotationX, 0, -20,0,-20, [20,20]), width , height)
+//gli occhi seguono fino ad un tot di spostamento
   image(imgsfondo, 0 , 0, width , height);
+  image(imgocchi, 0 + map(rotationY, -180, 180, -20, 20 ), 0 + map(rotationX, -180, 180, -20, 20 ) , width , height)
+  image(imgcover, 0 ,0 , width , height);
 }
 
 
